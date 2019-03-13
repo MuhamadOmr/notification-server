8d9fd0dda1b14e9edcd912cbbf8d1bc1397f10bf
muhamadomr <mohamed.ahmed.c209@gmail.com>
2019-03-13 05:57:38 +0200

    [update] add docker compose up support to start services

--- api/components/push/helpers/jobsMessageQueue.js
+++ api/components/push/helpers/jobsMessageQueue.js
@@ -1,8 +1,10 @@
+const config = require('config');
+const Config = JSON.parse(JSON.stringify(config));
 const Queue = require('bull');
 
 const TEN_SECONDS = 10000;
 
-const JobsQueue = new Queue('jobs message queue', 'redis://127.0.0.1:6379', {
+const JobsQueue = new Queue('jobs message queue', Config.redis, {
   // attemps for retry the job if it fails
   defaultJobOptions: { attempts: 10 },
   // options how to handle the retries
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Api notification backend!
+example of a backend soultion for sending push notifications and sms messages consists of :
+- Api service
+- Push notification service
+- SMS service
+
+# start services
+```sh
+docker-compose up --build
+```
+
+#### Api Endpoints Documentation
+```url
+localhost:3000/documentation
+```
